f5c8008b31641c4aa32c5caca8bf5edd836a28d2
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2020-04-24 21:18:32 +0200

    Connect to websocket

--- src/App.fsx
+++ src/App.fsx
@@ -1,3 +1,27 @@
+open Browser.Types
 #load "../.paket/load/netstandard2.0/main.group.fsx"
 
-printfn "It works!"
+
+open Browser.WebSocket
+open Fable.Core.JS
+
+let ws = WebSocket.Create "ws://localhost:61942"
+
+let onMessage (msg: MessageEvent) =
+    console.log(msg.data)
+
+let onOpen _ =
+    ws.send "{
+      \"messageType\": \"PIPELINE\",
+      \"commands\": [
+        {
+          \"messageType\": \"REPLAY\"
+        },
+        {
+          \"messageType\": \"SUBSCRIBE\"
+        }
+      ]
+    }"
+
+ws.addEventListener_message onMessage
+ws.addEventListener_open onOpen
