db2204d871f45e3c25480a16c49148924ccb3e54
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2020-06-14 17:59:01 +0200

    Create vendor chunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const {BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const path = require("path");
 
 const config = (ifDev, ifProd) => ({
     ...ifDev({
-        mode: 'development',
-        devtool: 'eval-source-map'
+        mode: "development",
+        devtool: "eval-source-map",
     }),
     ...ifProd({
-        mode: 'production',
-        devtool: 'source-map'
+        mode: "production",
+        devtool: "source-map",
     }),
     output: {
         path: path.join(__dirname, "./dist"),
-        filename: '[name].[hash].js',
+        filename: "[name].[hash].js",
     },
     devServer: {
         publicPath: "/",
@@ -27,32 +27,45 @@ const config = (ifDev, ifProd) => ({
         extensions: [".ts", ".js"],
     },
     module: {
-        rules: [{
-            test: /\.ts$/,
-            use: "ts-loader",
-        }]
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './public/index.html',
+            filename: "index.html",
+            template: "./public/index.html",
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static', 
+            analyzerMode: "static",
             openAnalyzer: false,
         }),
-    ]
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /node_modules/,
+                    chunks: "initial",
+                    name: "vendor",
+                },
+            },
+        },
+    },
 });
 
 module.exports = (prod = true) => config(ifEnv(!prod), ifEnv(prod));
 
 function ifEnv(active) {
-    return arrayOrObject => {
+    return (arrayOrObject) => {
         if (active) {
             return arrayOrObject;
         } else {
             return Array.isArray(arrayOrObject) ? [] : {};
         }
-    }
+    };
 }
